6e25fcda402ec74c0129fa289a9222dbe1468d08
Alvaro <alvaroph@gmail.com>
2024-01-17 09:52:59 +0100

    commit inicial

---
+++ src/components/cmpBebidas.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+     
+     </div>
+</template>
+
+<script>
+    export default {
+        name: "cmpBebidas",
+        data() {
+            return {
+                
+            }
+        },
+        props: {
+       
+        },
+       
+    }
+</script>
+
---
+++ src/components/cmpBuscador.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-btn v-for="item in ['A','B','C','D']" @click="recuperaBebidas(item)" v-bind:key="item.strDrink" >
+      {{ item }}
+    </v-btn>
+
+   
+</div>
+
+</template>
+
+<script>
+  
+  export default {
+    data() {
+      return {
+        bebidas: []
+      }
+    },
+    components: {
+   
+    },
+    methods: {
+      recuperaBebidas(letra) {
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f='+letra)
+          .then(response => response.json())
+          .then(data => this.bebidas=data.drinks)
+      }
+    }
+  }
+</script>
